52e7b693aebb28bd5183db26f012ecf195e21780
musman25831 <usman232429@gmail.com>
2022-09-18 00:17:48 +0500

    animation and Responsive fixes

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -12,33 +12,33 @@ const data=[{
 const Card = () => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <p className="mt-2 text-2xl font-medium	not-italic" > Meet The Team</p>
-      <div className="mt-6">
+      <p className="mt-2 text-2xl font-medium	not-italic  md:mt-2 text-lg text-center font-mono	not-italic  hover:text-tahiti " > Meet The Team</p>
+      <div className="mt-6  ">
         <img src={"/images/images1.jpg"} width={400} />
         <div >
-          <p>Monet Goode</p>
-          <p>Founder</p>
+          <p className="text-xl font-mono text-metal ">Monet Goode</p>
+          <p className="text-xl font-mono  text-bermuda">Founder</p>
         </div>
       </div>
       <div className="relative bottom-72 left-60">
-        <img src={"/images/images3.jpg"} width={400} />
-        <div >
-          <p>Audrius Linkus</p>
-          <p>Heade Of Interactive</p>
+        <img src={"/images/images3.jpg"} width={400}  class=" hover:animate-waving-hand  md:w-80 right-20" />
+        <div className=" text-center sm:text-left">
+          <p className="text-xl font-mono text-metal  sm:text-left">Audrius Linkus</p>
+          <p className="text-xl font-mono  text-bermuda">Heade Of Interactive</p>
         </div>
       </div>
-      <div className="-mt-60">
+      <div className="-mt-60 sm:text-left">
         <img src={"/images/images1.jpg"} width={400} />
-        <div >
-          <p>Monet Goode</p>
-          <p>Founder</p>
+        <div  >
+          <p className="text-xl font-mono text-metal">Monet Goode</p>
+          <p className="text-xl font-mono  text-bermuda">Founder</p>
         </div>
       </div>
       <div className="relative bottom-80 left-60">
-        <img src={"/images/images4.jpg"} width={400} />
-        <div >
-          <p>Audrius Linkus</p>
-          <p>Heade Of Interactive</p>
+        <img src={"/images/images4.jpg"} width={400}  class=" hover:animate-waving-hand md:w-80" />
+        <div className=" text-center sm:text-left" >
+          <p className="text-xl font-mono text-metal  sm:text-left">Audrius Linkus</p>
+          <p className="text-xl font-mono text-bermuda">Heade Of Interactive</p>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+    extend: {
+      keyframes: {
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(14deg)' },
+          '20%': { transform: 'rotate(-8deg)' },
+          '30%': { transform: 'rotate(14deg)' },
+          '40%': { transform: 'rotate(-4deg)' },
+          '50%': { transform: 'rotate(10.0deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
+        },
+      },
+      animation: {
+        'waving-hand': 'wave 2s linear infinite',
+      },
+
+    },
   },
   plugins: [],
 };
